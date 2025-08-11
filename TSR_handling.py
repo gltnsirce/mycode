487a0240016a0f286424541255fe9c5b1bd4d47d
Yiming <41096999+gltnsirce@users.noreply.github.com>
2025-08-11 23:35:50 +0800

    Update TSR_handling.py

--- TSR_handling.py
+++ TSR_handling.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-"""
-# Copyright (c) 2015 Dell Inc. or its subsidiaries. All Rights Reserved.
-#
-# This software contains the intellectual property of Dell Inc. or is licensed to Dell Inc. from third parties.
-# Use of this software and the intellectual property contained therein is expressly limited to the terms and
-# conditions of the License Agreement under which it is provided by or on behalf of Dell Inc. or its subsidiaries.
-
-#User Develop Guide
 
 ##1, how to use
   ###copy TSR  logs to node_review folder
